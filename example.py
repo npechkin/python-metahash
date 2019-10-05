f6d0008826da9c9bc1ca788a912916e0102d2293
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-05 10:32:42 +0300

    Update example.py

--- example.py
+++ example.py
@@ -7,7 +7,8 @@ import metahash
 ################################################
 net = 'main'
 address = '0x00ffaca83513356a14d91f95f46a40aa73b06d5f32359de1d0'
-#passwd = '12345' # for *.ec.priv
+passwd = '12345' # for *.ec.priv
+#passwd = getpass ("password: ")
 ##############  KEY GENERATE  ##################
 #priv_key = metahash.generate ()
 #################  KEY LOAD   ##################
@@ -18,9 +19,9 @@ address = '0x00ffaca83513356a14d91f95f46a40aa73b06d5f32359de1d0'
 #priv_key = metahash.get_prv_pem ( prv_pem_ascii )
 
 ## EC PEM private key
-#f = open ( address+".ec.priv" )
-#ec_prv_pem_ascii = f.read()
-#priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
+f = open ( address+".ec.priv" )
+ec_prv_pem_ascii = f.read()
+priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
 
 ## PEM public key
 #f = open ( address+".pub" )
@@ -28,8 +29,8 @@ address = '0x00ffaca83513356a14d91f95f46a40aa73b06d5f32359de1d0'
 #pub_key = metahash.get_pub_pem ( pub_pem_ascii )
 
 ## DER private key
-prv_der_ascii = '307402010104206ce96125549121883cdb8823a8f9d020249c58d638f92546744f652156ad6840a00706052b8104000aa14403420004f5e084f2ec9e16963b6fc22d687408f233cdbef42cf9d33a7cd8bdff6729d8f9b914c33bf337afcf144dd18bd3212fca05fb802b58253d474a146f1f33761ae1'
-priv_key = metahash.get_prv_der ( prv_der_ascii )
+#prv_der_ascii = '307402010104206ce96125549121883cdb8823a8f9d020249c58d638f92546744f652156ad6840a00706052b8104000aa14403420004f5e084f2ec9e16963b6fc22d687408f233cdbef42cf9d33a7cd8bdff6729d8f9b914c33bf337afcf144dd18bd3212fca05fb802b58253d474a146f1f33761ae1'
+#priv_key = metahash.get_prv_der ( prv_der_ascii )
 
 ## DER public key
 #pub_der_ascii = '3056301006072a8648ce3d020106052b8104000a03420004f5e084f2ec9e16963b6fc22d687408f233cdbef42cf9d33a7cd8bdff6729d8f9b914c33bf337afcf144dd18bd3212fca05fb802b58253d474a146f1f33761ae1'
@@ -97,13 +98,13 @@ priv_key = metahash.get_prv_der ( prv_der_ascii )
 
 ##################   SEND MHC   ###################
 
-to = '0x0088825ae25e516a34cb94bada9b25a811213b55ae3160c888'
-value = '0'
-fee = '0'
-nonce = metahash.fetch_balance( net, address )['result']['count_spent'] + 1
-data = ''
-res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
-result = json.dumps ( res )
-print ( result )
+#to = '0x0088825ae25e516a34cb94bada9b25a811213b55ae3160c888'
+#value = '0'
+#fee = '0'
+#nonce = metahash.fetch_balance( net, address )['result']['count_spent'] + 1
+#data = ''
+#res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
+#result = json.dumps ( res )
+#print ( result )
 
 ####################   END   ######################
