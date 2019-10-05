3abf4b1d0bc13309eb77111836843a04063fd94b
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-05 10:26:39 +0300

    Add files via upload

---
+++ undelegate_self.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import os
+import sys
+import json
+import metahash
+from getpass import getpass
+################################################
+net = 'main'
+address = '0x00ffaca83513356a14d91f95f46a40aa73b06d5f32359de1d0'
+#passwd = '12345' # for *.ec.priv
+passwd = getpass ("paasword: ")
+################################################
+# for Metagate Key
+f = open ( address+".ec.priv" )
+ec_prv_pem_ascii = f.read()
+priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
+################################################
+to = address
+value = '0'
+fee = '0'
+nonce = metahash.fetch_balance( net, address )['result']['count_spent'] + 1
+data = '{"method":"undelegate"}'
+res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
+result = json.dumps ( res )
+print ( result )
