2094b9b0de97a53bbfdd23baf9230673f7603361
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-06 10:17:58 +0300

    Update balance.py

--- examples/balance.py
+++ examples/balance.py
@@ -6,7 +6,7 @@ import metahash
 from getpass import getpass
 ################################################
 net = 'main'
-address = '0x008288d035e4b4b8a043b27dd4487e3440894eaaa3044de888'
+address = '0x00'
 ################################################
 balance = metahash.fetch_balance(net,address)['result']
 bal = (balance['received'] - balance['spent'])/1000000
