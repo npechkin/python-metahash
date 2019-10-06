c0aae9b51032530bad63ae816acf3afb7548c207
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-06 10:45:21 +0300

    Update sending.py

--- examples/sending.py
+++ examples/sending.py
@@ -4,22 +4,22 @@ import sys
 import json
 import metahash
 from getpass import getpass
+
 ################################################
 net = 'main'
-address = '0x00ffaca83513356a14d91f95f46a40aa73b06d5f32359de1d0'
+address = '0x00'
 passwd = '12345' # for *.ec.priv
 #passwd = getpass ("password:") # for *.ec.priv
 value = '0' # 1 MHC = 1000000
-##############  KEY GENERATE  ##################
-#priv_key = metahash.generate ()
+to = '0x0088825ae25e516a34cb94bada9b25a811213b55ae3160c888'
+
 #################  KEY LOAD   ##################
 ## EC PEM private key
 f = open ( address+".ec.priv" )
 ec_prv_pem_ascii = f.read()
 priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
-##################   SEND MHC   ###################
 
-to = '0x0088825ae25e516a34cb94bada9b25a811213b55ae3160c888'
+##################   SEND MHC   ###################
 fee = '0'
 nonce = metahash.fetch_balance( net, address )['result']['count_spent'] + 1
 data = ''
