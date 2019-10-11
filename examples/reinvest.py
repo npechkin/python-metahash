48447d4c8f71a93917d3d6487f658176a2313e3c
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-11 09:43:51 +0300

    Add files via upload

--- examples/reinvest.py
+++ examples/reinvest.py
@@ -9,21 +9,23 @@ from getpass import getpass
 net = 'main'
 address = '0x00'
 #passwd = '12345' # for *.ec.priv
-passwd = getpass ("password:") # for *.ec.priv
+passwd = getpass ("password: ") # for *.ec.priv
 node = '0x00f0bec7a7b832d4400455229103c6cec3abd6736f60152b6d'
+amount = '0' # 0 = ALL
 ################################################
-# for Metagate Key
-f = open ( address+".ec.priv" )
+f = open ( "keys/"+address+".ec.priv" )
 ec_prv_pem_ascii = f.read()
 priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
 ################################################
 to = node
 value = '0'
 fee = '0'
-
 balance = metahash.fetch_balance ( net, address )['result']
-bal = str(balance['received'] - balance['spent'])
-print ("balance =",bal)
+bal = balance['received'] - balance['spent']
+if bal == 0:
+    print ("Balance = 0. Exiting.")
+    sys.exit()
+print ("Balance =",bal,"MHC. Undelegating.")
 nonce = balance['count_spent'] + 1
 data = '{"method":"undelegate"}'
 res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
@@ -31,9 +33,14 @@ result = json.dumps ( res )
 print ( result )
 print ("Wait 5 second...")
 time.sleep (5)
-balance = metahash.fetch_balance ( net, address )['result']
-bal = str(balance['received'] - balance['spent'])
-print ("balance =",bal)
+if amount == '0':
+    balance = metahash.fetch_balance ( net, address )['result']
+    bal = balance['received'] - balance['spent']
+    if bal == 0:
+        print ("Balance = 0. Exiting.")
+        sys.exit()
+    amount = str(bal)
+print ("Balance =",bal,"MHC. Delegating.")
 nonce = nonce + 1
 data = '{"method":"delegate","params":{"value":"'+str(bal)+'"}}'
 res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
