0623807761ddf64fa01d5e44bac679a21266084a
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-11 09:00:59 +0300

    Add files via upload

--- examples/sending.py
+++ examples/sending.py
@@ -4,27 +4,29 @@ import sys
 import json
 import metahash
 from getpass import getpass
-
 ################################################
 net = 'main'
 address = '0x00'
 #passwd = '12345' # for *.ec.priv
 passwd = getpass ("password:") # for *.ec.priv
-value = '0' # 1 MHC = 1000000
-to = '0x0088825ae25e516a34cb94bada9b25a811213b55ae3160c888'
-
+to = '0x00'
+value = '0' # if 0 send ALL
 #################  KEY LOAD   ##################
-## EC PEM private key
-f = open ( address+".ec.priv" )
+f = open("keys/"+address+".ec.priv","r")
 ec_prv_pem_ascii = f.read()
 priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
-
 ##################   SEND MHC   ###################
-fee = '0'
-nonce = metahash.fetch_balance( net, address )['result']['count_spent'] + 1
+if value == '0':
+    balance = metahash.fetch_balance(net,address)['result']
+    bal = (balance['received'] - balance['spent'])
+    if bal == 0:
+        sys.exit()
+    value = str(bal)
+print ("Sending",value,"MHC to", to)
+fee = 0
+nonce = balance['count_spent'] + 1
 data = ''
 res = metahash.mhc_send ( net, to, value, fee, nonce, data, priv_key )
 result = json.dumps ( res )
 print ( result )
-
 ####################   END   ######################
