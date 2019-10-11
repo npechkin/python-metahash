380626d2360e0d973d37543c4aa31b5fa16f23b8
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-11 11:29:39 +0300

    Update delegate.py

--- examples/delegate.py
+++ examples/delegate.py
@@ -8,7 +8,7 @@ from getpass import getpass
 net = 'main'
 address = '0x00'
 #passwd = '12345' # for *.ec.priv
-passwd = getpass ("password:") # for *.ec.priv
+passwd = getpass ("password: ") # for *.ec.priv
 val = '0' # 1 MHC = 1000000; IF 0 THEN DELEGATE ALL
 node = '0x00f0bec7a7b832d4400455229103c6cec3abd6736f60152b6d'
 ################################################
@@ -19,14 +19,18 @@ priv_key = metahash.get_ec_prv_pem ( ec_prv_pem_ascii, passwd )
 ################################################
 to = node
 balance = metahash.fetch_balance ( net, address )['result']
-bal = str(balance['received'] - balance['spent'])
-if int(val) < 512000000:
-    val = bal
-if int(val) > int(bal):
-    val = bal
-if int(bal) < 512000000:
+bal = balance['received'] - balance['spent']
+if bal < 512000000:
     print ("Balance less than 512 MHC. Exit.")
     sys.exit ()
+if int(val) > 0 & int(val) < 512000000:
+    print ("You can not delegate less than 512 MHC. Exit.")
+    sys.exit ()
+if bal < int(val):
+    print ("Balance =",bal/1000000,"MHC.",val/1000000,"MHC cannot be sent. Exit.")
+    sys.exit()
+if val == '0':
+    val = str(bal)
 value = '0'
 fee = '0'
 nonce = balance['count_spent'] + 1
