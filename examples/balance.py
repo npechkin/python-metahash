fd4977d6cb6bc98d917012320fdddfb973173690
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-16 21:26:52 +0300

    Add files via upload

--- examples/balance.py
+++ examples/balance.py
@@ -12,5 +12,9 @@ balance = metahash.fetch_balance(net,address)['result']
 bal = (balance['received'] - balance['spent'])/1000000
 deleg = (balance['delegate'] - balance['undelegate'])/1000000
 nonce = balance['count_spent'] + 1
-#print (balance)
+beginTx = 0
+countTxs = 10
+delegations = metahash.get_address_delegations( net, address, beginTx, countTxs )['result']
+
 print ("Balance =",bal,"MHC. Frozen in delegations",deleg,"MHC. nonce =",nonce)
+print ("Delegations count",str(len(delegations)+1),"(from 256)")
