93a56f2313fa21c9b398797d79b5a964549fc5cf
npechkin <52535105+npechkin@users.noreply.github.com>
2019-10-18 12:35:16 +0300

    Update example.bat

--- examples/example.bat
+++ examples/example.bat
@@ -1,6 +1,6 @@
 cd D:\python\metahash\examples
 rem sending.py
 rem timeout /t 05 /nobreak > nul
-reinvest.py
-timeout /t 05 /nobreak > nul
+rem reinvest.py
+rem timeout /t 05 /nobreak > nul
 balance.py
